f5d1dfa3d2b7584efb1dd7aca054c99514c87c5d
Simon Cropp <simon.cropp@gmail.com>
2012-09-28 22:09:13 +1000

    bump version, make package project level

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ExtraConstraints")]
 [assembly: AssemblyProduct("ExtraConstraints")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
