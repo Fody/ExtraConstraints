4ef8116b4ee617b041ac787bd5ce3d631fbc7031
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:12:30 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("ExtraConstraints")]
 [assembly: AssemblyProduct("ExtraConstraints")]
-[assembly: AssemblyVersion("1.10.1")]
-[assembly: AssemblyFileVersion("1.10.1")]
+[assembly: AssemblyVersion("1.10.2")]
+[assembly: AssemblyFileVersion("1.10.2")]
